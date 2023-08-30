ea86085320749fa0aeb64043f41cba68b3a73893
l3on <l3onwebdev@gmail.com>
2023-08-30 16:49:40 +0200

    added search input

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -2,45 +2,53 @@
 
 
 @section('main')
-    <div class="container">
+    <div class="container mt-5">
 
+        <div class="d-flex justify-content-between align-items-center mb-5">
+            <a href="{{ route('students.create') }}" class="btn btn-success">Create</a>
+            <form method="GET" action="">
+                <div class="input-group">
+                    <input type="text" class="form-control" name="filter" placeholder="Name..." aria-label="Filter"
+                        value="{{ $filter ?? '' }}">
+                    <button type="submit" class="btn btn-outline-secondary" type="button">Search</button>
+                </div>
+            </form>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Classe</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($students as $student)
+                    <tr>
+                        <th scope="row">{{ $student['id'] }}</th>
+                        <td>{{ $student['name'] }}</td>
+                        <td>{{ $student['surname'] }}</td>
+                        <td>100</td>
+                        <td class="d-flex">
+                            <a href="" class="btn btn-primary me-1">View</a>
+                            <a href="{{ route('students.edit', $student) }}" class="btn btn-warning me-1">Edit</a>
+                            {{-- Delete Button --}}
+                            <form action="{{ route('students.destroy', $student) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
 
-   <a href="{{route('students.create')}}" class="btn btn-success mt-5">Create</a>
-    <table class="table mt-5">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Classe</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-            @forelse ($students as $student)
-            <tr>
-              <th scope="row">{{$student['id']}}</th>
-              <td>{{$student['name']}}</td>
-              <td>{{$student['surname']}}</td>
-              <td>100</td>
-              <td class="d-flex">
-                   <a href="" class="btn btn-primary me-1">View</a>
-                        <a href="{{ route('students.edit', $student) }}" class="btn btn-warning me-1">Edit</a>
-                     {{-- Delete Button --}}
-                <form action="{{ route('students.destroy', $student) }}" method="POST">
-                  @csrf
-                  @method('DELETE')
-                  <button type="submit" class="btn btn-danger">Delete</button>
-              </form>
-              </td>
-            </tr>
-                
-            @empty
-                <h3>No Students Available</h3>
-            @endforelse
+                @empty
+                    <h3>No Students Available</h3>
+                @endforelse
 
-        </tbody>
-      </table>
+            </tbody>
+        </table>
 
     </div>
 @endsection
