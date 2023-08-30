c79a032cfcf0b36e67e4c94ba1eefb149e312ce9
stefansimereaa <stefanosimerea16@gmail.com>
2023-08-30 16:52:27 +0200

    added header to main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,6 +10,7 @@
 </head>
 
 <body>
+    @include('../partials/header')
     <main>
         @yield('main')
     </main>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <nav class="navbar bg-body-tertiary mt-5 ms-2">
+        <div class="container-fluid">
+            <h3>Search a student</h3>
+            <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+</div>
