4b5c2d2f4228ce893587527951fd70053d48ec42
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-30 12:39:22 +0200

    show card add

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -2,35 +2,14 @@
 
 
 @section('main')
-    <div class="container">
+    <div class="container mt-5">
 
-        <a href="" class="btn btn-success mt-5">Create</a>
-        <table class="table mt-5">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row"></th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="d-flex">
-                        <a href="" class="btn btn-primary me-1">View</a>
-                        <a href="" class="btn btn-warning me-1">Edit</a>
-                        <form action="" method="POST">
-                            <button href="" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <h1 class="mt-3 pb-3">Student info:</h1>
+
+        <div class="card w-75 p-3">
+            <h3 class="d-inline">Name:</h3>
+            <h3 class="d-inline">Surname:</h3>
+        </div>
 
     </div>
 @endsection
