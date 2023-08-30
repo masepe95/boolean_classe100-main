23e4ac9f98da3639d3eed4396f017e56de8d8184
l3on <l3onwebdev@gmail.com>
2023-08-30 15:35:54 +0200

    added action to form

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -6,16 +6,16 @@
 
         <h2 class="mb-3">Create Student</h2>
 
-        <form method="POST" action="">
+        <form method="POST" action="{{ route('students.store') }}">
             @csrf
             <div class="row">
-                <div class="col-7">
+                <div class="col-12">
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
                         <input type="text" class="form-control" id="name" name="name" placeholder="Name">
                     </div>
                 </div>
-                <div class="col-7">
+                <div class="col-12">
                     <div class="mb-3">
                         <label for="surname" class="form-label">Surname</label>
                         <input type="text" class="form-control" id="surname" name="surname" placeholder="Surname">
