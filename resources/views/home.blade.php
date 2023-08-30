c0fb4ba835b43c2100d18702052704a02ed03856
masepe95 <marcosepe10@gmail.com>
2023-08-30 11:31:53 +0200

    Project ready to be worked - index created

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+
+ @section('main')
+<h2><a href="{{ route('students.index') }}">Home</a></h2>
+    <a href="#">Portfolio</a>
+@endsection
---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.main')
+
+
+ @section('main')
+<div class="container">
+
+   <a href="" class="btn btn-success mt-5">Create</a>
+    <table class="table mt-5">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"></th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="d-flex">
+                <a href="" class="btn btn-primary me-1">View</a>
+                <a href="" class="btn btn-warning me-1">Edit</a>
+                <form action="" method="POST">
+                    <button href="" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+</div>
+
+@endsection
